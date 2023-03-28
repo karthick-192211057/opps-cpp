ce6c7ef299cb22cee6d2355353ffca985f1ead4e
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-03-28 19:52:54 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int n;
+	cout<<"enter a number:";
+	cin>>n;
+	if(n%2==0)
+	cout<<"it is even number";
+	else
+	cout<<"it is odd number";
+}

8eebc7e2baf68e6cb0d8577bc65f85272a576d6a
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-03-28 20:10:01 +0530

    Add files via upload

---
+++ area of rec and cir.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int l,b,r;
+	cout<<"enter length and breadth to find area of rectangle:";
+	cin>>l>>b;
+	cout<<"area of rectangle:"<<l*b;
+	cout<<"\nenter radius to find area of circle:";
+	cin>>r;
+	cout<<"\narea of circle:"<<3.14*r*r;
+}

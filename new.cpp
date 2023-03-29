8243eb9c934e15a61cccc2423f9d5f2b1ae673d4
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-03-29 21:46:08 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,24 @@
+c code:
+#include <stdio.h> 
+int main(void) 
+{ 
+	int new = 5;
+	printf("%d", new); 
+}
+
+
+
+
+
+
+
+ans c++ code:
+
+using namespace std;
+#include<iostream>
+int main() 
+{ 
+	int neww=5;
+	cout<<neww; 
+	return 0; 
+}

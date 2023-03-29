571ecb942725df3aaeb8f9a6b31f0954b3071f01
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-03-29 21:40:43 +0530

    Add files via upload

---
+++ assign3.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int i=10, j=5;
+	int modresult=0;
+	int divresult=0;
+	modresult=i%j;
+	cout<<modresult;
+	divresult=i/modresult;
+	cout<<divresult;
+	return 0;
+}
+
